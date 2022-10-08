796e4d7b821fcad308d106139cec488aa974149d
yzzhang <yuzhu_cn@163.com>
2022-10-08 21:46:57 +0800

    base bc core lib

--- base/src/androidTest/java/com/train/base/ExampleInstrumentedTest.kt
+++ base/src/androidTest/java/com/train/base/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.train.bouncycastle
+package com.train.base
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.train.bouncycastle.test", appContext.packageName)
+        assertEquals("com.train.base.test", appContext.packageName)
     }
 }
---
+++ docs/Tls Open Source.md
@@ -0,0 +1,11 @@
+
+
+1. OpenSSL 
+
+https://zhuanlan.zhihu.com/p/28850798?force_isolation=true
+
+https://www.cnblogs.com/Jack-Blog/p/13170728.html#hello-request
+
+https://blog.csdn.net/weixin_43364172/article/details/83988580
+
+https://mbd.baidu.com/newspage/data/landingsuper?context=%7B%22nid%22%3A%22news_10007393964393896475%22%7D&n_type=-1&p_from=-1
